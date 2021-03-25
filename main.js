6c608f4f9270d98a1fead76da0fd2f91d5f47c95
GhostAgen <79643880+GhostAgen@users.noreply.github.com>
2021-03-25 21:23:25 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+menu_list_array =["Chicken Tandoori Pizza,Veg Pizza,Veg Supreme Pizza,Paneer Tikka Pizza,Deluxe Veggie Pizza,Veg Extravaganza Pizza"]
+function getmenu(){
+    var htmldata;
+    htmldata="<ol class='menulist'>"
+    menu_list_array.sort();
+    for(i=0;i<menu_list_array.length;i++){
+        htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+    }
+    htmldata = htmldata +"</ol>"
+document.getElementById("display_menu").innerHTML = htmldata;
+}
+function add_item(){
+    var htmldata;
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+    menu_list_array.sort();
+    htmldata="<section class='cards'>"
+    htmldata=htmldata+'<li>' + menu_list_array[i] + '</li>'
+htmldata=htmldata+'<div class="card>'+'<img src="images/pizzaIMG.png"/>' + menu_list_array[i]
+}
+htmldata=htmldata+"</section>"
+document.getElementById("display_addedmenu").innerHTML = htmldata;
+}
